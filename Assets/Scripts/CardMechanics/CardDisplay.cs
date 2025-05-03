565e19e25b31bf35e1c3b25a6342fa5c9db27fac
emmanuel cyr <emmanuel.cyr159@gmail.com>
2025-05-03 15:07:17 -0400

    Core mechanic nearly done + animancer package

--- Assets/Scripts/CardMechanics/CardDisplay.cs
+++ Assets/Scripts/CardMechanics/CardDisplay.cs
@@ -1,15 +1,32 @@
 using TMPro;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class CardDisplay : MonoBehaviour
 {
+    [SerializeField] public bool isFlipped = false;
+
     [SerializeField] public Card card;
 
     [SerializeField] public GameObject hintObject;
+
+    public delegate void FlipCard(Card card);
+
+    public static event FlipCard flipCard;
     public void ShowHint()
     {
         hintObject.SetActive(true);
         hintObject.GetComponentInChildren<TMP_Text>().text = card.hint;
+        flipCard (card);
+        TurnCard();
+    }
+
+    public void TurnCard()
+    {
+        isFlipped = !isFlipped;
+
+        if (isFlipped) { GetComponent<Image>().color = Color.red; }
+        else { GetComponent<Image>().color = Color.white; }
     }
 }
 
