fede4d5b5e5f92b516b8559fed774c216a47dd76
Audric Ligtas <audric.ligtas@gmail.com>
2025-05-04 10:08:34 -0400

    Dictionary function addition

--- Assets/Scripts/Dictionary.cs
+++ Assets/Scripts/Dictionary.cs
@@ -1,9 +1,21 @@
+using System;
 using UnityEngine;
 
 public class Dictionary : MonoBehaviour
 {
+    [SerializeField] public Canvas canvas;
+
+    [SerializeField] public int chosen = 0;
+    [SerializeField] public Canvas DE;
+    public void HideAll()
+    {
+
+    }
     public void DemEva()
     {
-        
+        if (chosen == 0)
+        {
+
+        }
     }
 }
